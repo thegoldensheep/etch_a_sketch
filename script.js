2d3b93ed37aefa465c7df735b50008b462ff62a3
thegoldensheep <dillonshepherd1991@gmail.com>
2019-07-29 02:02:28 -0400

    Changed Black Button to color picker, added choose color functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentColumnCount = 20;
 let currentMode = 1;
+let currentPenStyle = 'background-color: rgb(0, 0, 0);';
 
 function setupGrid(rowColumnCount){
   let gridDiv = document.getElementById('grid');
@@ -34,7 +35,7 @@ function setupGrid(rowColumnCount){
 
 function mouseOver(){
   if(currentMode === 1){
-    this.style = 'background-color: rgb(0,0,0);'
+    this.style = currentPenStyle;
   } else if(currentMode === 2) {
     this.style = 'background-color: rgb('+Math.random()*255+','+Math.random()*255+','+Math.random()*255+');'
   }else if(currentMode === 3){
@@ -73,6 +74,29 @@ function resetGrid(){
   }
 
 
+}
+
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function updatePenColor(){
+  currentPenValue = document.getElementById("penButton").value;
+  let r = hexToRgb(currentPenValue).r;
+  let g = hexToRgb(currentPenValue).g;
+  let b = hexToRgb(currentPenValue).b;
+  currentPenStyle = 'background-color: rgb('+r+','+g+','+b+')';
 }
 
 setupGrid(currentColumnCount);
